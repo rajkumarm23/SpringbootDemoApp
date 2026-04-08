f688e36c08b503baebb0e56441f56d2b1a8f6bc0
Mekala Rajkumar <rajkumarmekala1@gmail.com>
2026-04-08 15:18:20 +0530

    Initial Spring Boot app

---
+++ src/main/java/com/accenture/DempApplication/HelloController.java
@@ -0,0 +1,14 @@
+package com.accenture.DempApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot + Jenkins!";
+    }
+}
+
